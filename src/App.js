da4e47deb9f82a16902a9874af0effd0aa3a50e6
Mohammed Alfakir <its.mohammedalfakir@gmail.com>
2026-04-15 16:51:40 +0100

    Update configurator-previewer to version 1.0.6 and clean up App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import { ConfiguratorPreviewDialog } from "configurator-previewer";
-// import myConfig from "./configurator-v1.0.0.json";
 
 function App() {
   return (
     <>
-      {/* <ConfiguratorPreviewDialog configuratorJson={myConfig} /> */}
-
       <ConfiguratorPreviewDialog
         configuratorJson="/configurator-v1.0.0.json"
         imagePrefix="https://imagedelivery.net/aYYmWUcv7lRhpLdU4ojPsA/"
         imageSuffix="/public"
+        onVariableSetChange={vars => {
+          console.log(vars);
+        }}
       />
-
-      {/* <ConfiguratorPreviewDialog configuratorJson={} /> */}
     </>
   );
 }
